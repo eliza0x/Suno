caf466833aa0274d27ded17299af584255504b30
eliz40x <eliz40x@gmail.com>
2016-07-26 22:01:28 +0900

    Modify Control.h: fix useless func

--- src/Control.h
+++ src/Control.h
@@ -1,4 +1,3 @@
-/* サーボのヘッダファイルを準備 */
 #include <Servo.h>
 
 /* サーボモータの制御クラス */
@@ -6,28 +5,15 @@ class ServoIO{ //{{{
 
   Servo servo; /* サーボのピン */
 
-  /* サーボモータが可動範囲外に回転するのを防ぐ関数*/
-  int angleCalc(const int currentAngle, const int difference){
-    const int sum = currentAngle + difference;
-    if(sum < 5){return 0;}      /* 5度以下なら0度を返す */
-    if(sum > 175){return 180;}  /* 175度以上なら180度を返す */
-    return currentAngle + difference; 
-  }
-
   protected:
     /* サーボを回転させる関数 */
     /* 値は0~180の間で */
-    void rotateServo(const int difference){
-      const int currentAngle = servo.read();
-      const int changedAngle = angleCalc(currentAngle, difference);
-      if(currentAngle != changedAngle){
-        servo.write(changedAngle);
-      }
+    void rotateAngle(const int angle){
+      servo.write(angle);
     }
 
   public:
     /* サーボモータのピンを設定する関数 */
-    /* 無くても良いが、あると統一感が生まれる */
     void setServoPin(const int pin){
       servo.attach(pin);
     }
@@ -36,50 +22,26 @@ class ServoIO{ //{{{
 /* モータの制御クラス */
 class MotorIO{ //{{{
 
-  /* 優先度は Brake > Stop > Otherewise */
-  bool isMoveFoward; /* 前進するか */
-  bool isStop;       /* 停止するか */
-  bool isBrake;      /* ブレーキするか */
-
-  void brake(){/* Brake code here... */}
-  void stop(){/* Stop code here... */}
-  void moveFoward(){/* Move Foward code here... */}
-  void moveBackward(){/* Move backward code here... */}
+  struct Motor{
+    int firstPin;
+    int secondPin;
+  };
+  Motor motor = {LOW, LOW};
+  
+  void setMotorState(const int fst, const int snd){
+    digitalWrite(motor.firstPin, fst);
+    digitalWrite(motor.secondPin, snd);
+  }
 
   public:
-    void setMoveFoward(){ /* 前進するよう状態を変更 */
-      isMoveFoward = true;
-      isStop = false;
-      isBrake = false;
-    }
-    void setMoveBackward(){ /* 後退するよう状態を変更 */
-      isMoveFoward = false;
-      isStop = false;
-      isBrake = false;
-    }
-    void setStop(){ /* 停止するよう状態を変更 */
-      isStop = true;
-    }
-    void setBrake(){ /* ブレーキするよう状態を変更 */
-      isBrake = true;
-    }
-
-    void updateMotorState(){ /* モータの状態を更新 */
-       if(isBrake){
-        brake();
-        return;
-      }
-      if(isStop){
-        stop();
-        return;
-      }
-      if(isMoveFoward){
-        moveFoward();
-        return;
-      } else {
-        moveBackward();
-        return;
-      }     
+    void brake(){setMotorState(HIGH,HIGH);}
+    void stop(){setMotorState(LOW,LOW);}
+    void moveFoward(){setMotorState(HIGH,LOW);}
+    void moveBackward(){setMotorState(LOW,HIGH);}
+    void setMotorPin(const int fstPin, const int sndPin){
+      motor = {fstPin, sndPin};
+      pinMode(fstPin, OUTPUT);
+      pinMode(sndPin, OUTPUT);
     }
 };//}}}
 
@@ -91,22 +53,7 @@ class Control: public ServoIO, public MotorIO{ //{{{
     digitalWrite(errorPin, HIGH); 
   }
 
-  /* moveFooBarの抽象化関数 */
-  void setAngleAndDirection(const int angle, const bool isFoward){
-    rotateServo(angle);
-    if(isFoward){setMoveFoward();}else{setMoveBackward();}
-  }
-  
   public:
-    /* 左前に移動するよう状態を変更 */
-    void setMoveLeftFoward(){setAngleAndDirection(-30, true);}
-    /* 右前に移動するよう状態を変更 */
-    void setMoveRightFoward(){setAngleAndDirection(30, true);}
-    /* 左後に移動するよう状態を変更 */
-    void setMoveLeftBackward(){setAngleAndDirection(-30, false);}
-    /* 右後に移動するよう状態を変更 */
-    void setMoveRightBackward(){setAngleAndDirection(30, false);}
-    
     /* エラーLEDのピンを設定する関数 */
     void setErrorPin(const int pin){
       errorPin = pin;
