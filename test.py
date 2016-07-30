d7c9efe384a0e171984eab369a18d0110a5bb4b5
eliz40x <eliz40x@gmail.com>
2016-07-30 21:37:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,10 +3,7 @@ import httplib
 import time
 
 sleep_time = 1
-
 urllib2.urlopen('http://192.168.24.67/Forward/Right')
-time.sleep(sleep_time)
-urllib2.urlopen('http://192.168.24.67/Backward/Right')
 time.sleep(sleep_time)
 urllib2.urlopen('http://192.168.24.67/Newtral/Right')
 time.sleep(sleep_time)
@@ -30,11 +27,5 @@ time.sleep(sleep_time)
 urllib2.urlopen('http://192.168.24.67/Brake/Center')
 time.sleep(sleep_time)
 
-urllib2.urlopen('http://192.168.24.67/Forward/Brake')
-time.sleep(sleep_time)
-urllib2.urlopen('http://192.168.24.67/Backward/Brake')
-time.sleep(sleep_time)
-urllib2.urlopen('http://192.168.24.67/Newtral/Brake')
-time.sleep(sleep_time)
 urllib2.urlopen('http://192.168.24.67/Brake/Brake')
 time.sleep(sleep_time)
