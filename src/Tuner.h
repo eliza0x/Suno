7ada67f71293c9bee818d552b20e7c3b9d6a9737
eliz40x <eliz40x@gmail.com>
2016-07-30 15:22:15 +0900

    Fix server bug

--- src/Tuner.h
+++ src/Tuner.h
@@ -5,54 +5,26 @@ WiFiServer server(80);
 WiFiClient client;
 
 class Tuner {
-  
-  Order reqToOrder(const String req){
-    int dir;
-    int ang;
-    Order order;
-    
-    if(req.indexOf("Foward") != -1 ){
-      dir = 0;
-    } else if(req.indexOf("Backward") != -1 ){
-      dir = 2;
-    }
-
-    if(req.indexOf("Right") != -1 ){
-      ang = 0;
-    } else if(req.indexOf("Left") != -1 ){
-      ang = 2;
-    }
-    order.angle = ang;
-    order.direction = dir;
-    return order;
-  }
-
+ 
   public:
-
     void wakeup(const char* ssid, const char* password) {
       Serial.begin(9600);
-     
-      delay(500);
+      delay(1500);
       Serial.println();
       Serial.print("Connecting to ");
       Serial.print(ssid);
-      
       WiFi.begin(ssid, password);
-      
       while (WiFi.status() != WL_CONNECTED) {
-        delay(300);
+        delay(500);
         Serial.print(".");
       }
       Serial.println("");
-      Serial.println("WiFi connected");
-      
       server.begin();
       Serial.println("Server started");
-    
       Serial.println(WiFi.localIP());
     }
     
-    Order fetchOrder(){
+    String fetchOrder(){
       client = server.available();
       String req = "Nop";
       while(client){
@@ -63,6 +35,6 @@ class Tuner {
           client.stop();
         }
       }
-      return reqToOrder(req);
+      return req;
     }
 };
--- src/src.ino
+++ src/src.ino
@@ -1,19 +1,17 @@
 #include "Tuner.h"
-#include "Control.h"
-#include "Order.h"
+// #include "Control.h"
+// #include "Order.h"
 
 Tuner tuner;
-Control ctrl;
+// Control ctrl;
 
 void setup(){
   tuner.wakeup("Buffalo-G-5040", "ww5xmrmwh7kxn");
+  delay(3000);
   Serial.begin(9600);
-  ctrl.setErrorPin(12);
-  ctrl.setServoPin(13);
-  ctrl.setMotorPin(4,5);
 } 
 
 void loop(){
-  ctrl.move(tuner.fetchOrder());
-  delay(300);
+  Serial.println(tuner.fetchOrder());
+  delay(500);
 }
