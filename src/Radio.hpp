67f083071273f046e3b2b5fc4261cbe75235308e
eliz40x <me@eliza.link>
2016-10-16 20:11:09 +0900

    Update Control Panel

--- src/Radio.hpp
+++ src/Radio.hpp
@@ -33,7 +33,7 @@ class Radio {
           req = client.readStringUntil('\n');
           Serial.println(req);
           delay(1);
-          client.println(req);
+          client.println("<html><head><style>body{margin:50px;font-size:20px;font-weight:lighter;}a{color:#444;text-decoration:none;}ul{display:flex;justify-content:center;}li{border:solid 1px #222;border-radius:10px;margin:5px;padding:20px;list-style:none;width:70px;text-align:center;}</style></head><body><ul><li><a href=\"/Move/Right\">LeftF</a></li><li><a href=\"/Move/Right\">Forward</a></li><li><a href=\"/Move/Right\">RightF</a></li></ul><ul><li><a href=\"/Left\">Left</a></li><li><a href=\"/Stop\">Stop</a></li><li><a href=\"/Right\">Right</a></li></ul></body></html>");
           client.stop();
         }
       }
